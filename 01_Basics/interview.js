106708ee15189b8c29c9be9aca119b4e305c48fa
DeepKansara1212 <deepkansara1212@gmail.com>
2024-05-29 21:57:29 +0000

    interview

--- 01_Basics/interview.js
+++ 01_Basics/interview.js
@@ -15,10 +15,10 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123') 
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const bigNumber = 31547324124567986n
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 // Non-Primtive Datatypes (Call By Reference)
     // Arrays, Objects, Functions
@@ -32,7 +32,36 @@ let myObj = {
 
 const myFunction = function(){
     console.log("Hello World");
-}
+} 
 
 // Refer this document for types of datatype:
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ********************************* MEMORY ****************************************
+
+// Two types: 
+    // Stack (Primitive) 
+        // When memory is define in stack, then it make the copy of variable
+    // Heap (Non-Primitive) 
+        // When memory is define in Heap, then it will give the reference to the original value
+
+let myName = "Deep Kansara"
+
+let anotherName = myName
+anotherName = "Manan Patel" 
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@email.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "user2@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
