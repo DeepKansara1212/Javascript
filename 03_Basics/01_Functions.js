266a0544369246e3015038bca0c491b4779484f6
DeepKansara1212 <deepkansara1212@gmail.com>
2024-06-01 10:16:26 +0000

    functions

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -42,6 +42,48 @@ function loginUserMsg(username = "Sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMsg("ABC"))
+// console.log(loginUserMsg("ABC"))
 // console.log(loginUserMsg(""))
-console.log(loginUserMsg()) 
+// console.log(loginUserMsg()) 
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return `Value for val1: ${val1} \nValue for val2: ${val2} \nValue for num: ${num1}`
+}
+
+// console.log(calculateCartPrice(200)) 
+
+// But when we pass more than one arguments then it will only print first parameter, because we only gave one parameter
+// So for that we use ...num1 instead of num1 for the parameter 
+// And it will return values in the Array
+// ...num1 is known as REST operator  
+
+// console.log(calculateCartPrice(200, 300, 400, 500, 600)) 
+
+
+
+const user = {
+    username: "Deep",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+} 
+
+// handleObject(user) 
+
+// Object pass in function
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+// Array pass in function
+const myNewArr = [200, 400, 600, 800]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArr));
