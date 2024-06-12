0538850a5bdb1a0d7448f2b2b5be489eab73ef6e
DeepKansara1212 <deepkansara1212@gmail.com>
2024-06-12 07:36:46 +0000

    Static Property

---
+++ 10_OOPs/07_Static-prop.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const deep = new User("Deep")
+// console.log(deep.createId()) 
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+// console.log(iphone.createId())
